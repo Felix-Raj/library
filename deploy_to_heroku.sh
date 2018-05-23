6068d99fbc43635392c404546d00926845e5d622
Felix-Raj <mailtofelixraj@gmail.com>
2018-05-24 00:12:26 +0530

    ch in gitignore

---
+++ deploy_to_heroku.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+python manage.py makemigrations
+git add -f */migrations/*
+git commit -m "add migrations for heroku"
+git push heroku
+heroku run python manage.py migrate
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+git commit -m "removed migrations files"
