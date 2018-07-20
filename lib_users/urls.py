83ca738bb16a3306a16dd51720811d1098778125
Felix-Raj <mailtofelixraj@gmail.com>
2018-07-20 22:34:25 +0530

    * modify url

--- lib_users/urls.py
+++ lib_users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('new/', LibUsersCreateView.as_view(), name='lib_user_create'),
     path('lent/', LentListView.as_view(), name='lent_list'),
     path('lent/new/', LentCreateAPI.as_view(), name='lent_create'),
-    path('lent/return/<int:pk>', LentReceivedAPI.as_view(), name='lent_receive'),
+    path('lent/return/<int:pk>/', LentReceivedAPI.as_view(), name='lent_receive'),
 ]
