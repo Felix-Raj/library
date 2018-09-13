2755b134dbc28b1f77e32583a9d95d48e60a4be0
Felix-Raj <mailtofelixraj@gmail.com>
2018-09-13 15:19:46 +0530

    changed views in notes to user ViewSet, removed other views. urls now use router to use viewset

--- note/views.py
+++ note/views.py
@@ -1,17 +1,9 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from note.models import Note
 from note.serializers import NoteSerializer
 
 
-class NoteListAPI(ListAPIView):
-    serializer_class = NoteSerializer
+class NoteModelViewSet(ModelViewSet):
     queryset = Note.objects.all()
-
-
-class NoteCreateAPI(CreateAPIView):
     serializer_class = NoteSerializer
-
-class NoteDeleteAPI(DestroyAPIView):
-    serializer_class = NoteSerializer
-    queryset = Note.objects.all()
