1702ef848c7bde1759b37462884256ff92c41e2a
Felix-Raj <mailtofelixraj@gmail.com>
2018-09-22 21:59:59 +0530

    fix an error in url

--- lib_users/urls.py
+++ lib_users/urls.py
@@ -8,8 +8,8 @@ from lib_users.views import (LentListView, LentCreateAPI, LibUserListView,
 app_name = 'lib_users'
 urlpatterns = [
     path('', LibUserListView.as_view(), name='lib_user_list'),
-    path('<int:pk>/activate/', AccountActivateDeactivateAPI.as_view(), name='lib_user_activate'),
-    path('<int:pk>/deactivate/', AccountActivateDeactivateAPI.as_view(), name='lib_user_de_activate'),
+    path('<int:pk>/<str:state>/', AccountActivateDeactivateAPI.as_view(), name='lib_user_activate'),
+    path('<int:pk>/<str:state>/', AccountActivateDeactivateAPI.as_view(), name='lib_user_de_activate'),
     path('birthdays/', BirthdayAlertAPI.as_view(), name='lib_user_birthdays'),
     path('new/', LibUsersCreateView.as_view(), name='lib_user_create'),
     path('lent/', LentListView.as_view(), name='lent_list'),
